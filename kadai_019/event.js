92869eb157f85ae2f7bec4ca16fa44cf5de48721
ikuzeyo <134299001+ikuzeyo@users.noreply.github.com>
2023-09-19 01:19:26 +0900

    kadai_019をアップロード

---
+++ kadai_019/event.js
@@ -0,0 +1,17 @@
+
+const countBtn = document.getElementById('btn');
+
+
+
+
+countBtn.addEventListener('click', () => {
+    const reText=document.getElementById('text');
+    reText.textContent ='ボタンをクリックされました';
+
+
+});
+
+
+
+
+
